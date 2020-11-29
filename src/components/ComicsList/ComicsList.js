c7819f757b39049a46975c9aa20cac7c8ff3b312
Slatron <slatron@gmail.com>
2020-11-28 20:28:30 -0600

    updatre

--- src/components/ComicsList/ComicsList.js
+++ src/components/ComicsList/ComicsList.js
@@ -17,7 +17,7 @@ const ComicsList = (props) => {
     ) : <div className="loading">loading...</div>
 
   return (
-    <ul class="comics-list">{comicsListDOM}</ul>
+    <ul className="comics-list">{comicsListDOM}</ul>
   )
 }
 
