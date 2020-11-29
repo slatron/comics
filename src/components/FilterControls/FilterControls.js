2c08353af91502572e33d14e6b9c7cb95b7da3ef
Slatron <slatron@gmail.com>
2020-11-28 20:24:42 -0600

    update filter controls

--- src/components/FilterControls/FilterControls.js
+++ src/components/FilterControls/FilterControls.js
@@ -4,34 +4,37 @@ import './FilterControls.scss'
 const FilterControls = (props) => {
   return (
     <div className="filter-controls">
-      <select value={props.filterDate} onChange={props.handleFilterDateChange}>
-        <option value="lastWeek">Last Week</option>
-        <option value="thisWeek">This Week</option>
-        <option value="nextWeek">Next Week</option>
-        <option value="thisMonth">This Month</option>
-      </select>
-      {props.comicResults.length
-        ? <button
-            className={`${props.activeFiler === '(Variant)' ? 'active' : ''}`}
-            onClick={() => props.filterComics('(Variant)')}>
-            Variants
-          </button>
-        : null
-      }
-      {props.comicResults.length
-        ? <button
-            className={`${props.activeFiler === '(Trade Paperback)' ? 'active' : ''}`}
-            onClick={() => props.filterComics('(Trade Paperback)')}>
-            Trades
-          </button>
-        : null
-      }
-      {props.comicResults.length
-        ? <button
-        className={`${props.activeFiler === '(all)' ? 'active' : ''}`}
-            onClick={props.resetComics}>all</button>
-        : null
-      }
+      <h6 onClick={props.toggleFilterOpen}>+ Filter Results</h6>
+      <div className={`filter-controls-body ${props.filterOpen ? 'active' : ''}`}>
+        <select
+          value={props.filterDate}
+          onChange={props.handleFilterDateChange}>
+          <option value="lastWeek">Last Week</option>
+          <option value="thisWeek">This Week</option>
+          <option value="nextWeek">Next Week</option>
+          <option value="thisMonth">This Month</option>
+        </select>
+        {props.comicResults.length
+          ? <>
+              <div
+                className={`switch ${props.activeFiler === '(all)' ? 'active' : ''}`}
+                onClick={props.resetComics}>
+                all
+              </div>
+              <div
+                className={`switch ${props.activeFiler === '(Variant)' ? 'active' : ''}`}
+                onClick={() => props.filterComics('(Variant)')}>
+                Variants
+              </div>
+              <div
+                className={`switch ${props.activeFiler === '(Trade Paperback)' ? 'active' : ''}`}
+                onClick={() => props.filterComics('(Trade Paperback)')}>
+                Trades
+              </div>
+            </>
+          : null
+        }
+      </div>
     </div>
   )
 }
