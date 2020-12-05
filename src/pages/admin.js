c1744c1c1efa12ecbc6b4b2059c1dc9a72c2a0b4
Slatron <slatron@gmail.com>
2020-12-05 11:14:14 -0600

    adds admin pages

---
+++ src/pages/admin.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AdminPage from 'components/page_roots/AdminPage/AdminPage'
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import allReducer from 'store/reducers'
+
+import 'styles/main.scss'
+
+const store = createStore(allReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AdminPage />
+  </Provider>,
+  document.getElementById('admin-page'))
