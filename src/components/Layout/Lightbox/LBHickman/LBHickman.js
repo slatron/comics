61329faf00f6c78ac9c50685d287ee2b6553dc70
Mike Slater <slatron@gmail.com>
2021-01-12 20:47:08 -0600

    styles for hickman lightbox

--- src/components/Layout/Lightbox/LBHickman/LBHickman.js
+++ src/components/Layout/Lightbox/LBHickman/LBHickman.js
@@ -2,17 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {characterData} from 'components/page_roots/HickmanPage/characterData'
 
+import './LBHickman.scss'
+
 const LBDetails = ({details}) => {
   const info = characterData[details.character]
 
   return (
-    <div className="lb-details">
+    <div className="lb-hickman">
       <h3>{info.name}</h3>
       <h4>Identity: {info.identity}</h4>
       <h4>First Appearance: {info.first_appearance}</h4>
-      <h4>Description:
+      <section>
         <span dangerouslySetInnerHTML={{ __html: info.description }}></span>
-      </h4>
+      </section>
     </div>
   )
 }
