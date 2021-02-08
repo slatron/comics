0b8546b1da8e48389ce5be894320d5d0561e6e2e
Slatron <slatron@gmail.com>
2021-02-08 14:54:43 -0600

    updating IE11 build

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -18,9 +18,6 @@ const Drawer = (props) => {
         <li className={`${props.section === 'comics' ? 'active' : ''}`}>
           <a href="./comics.html">Comics</a>
         </li>
-        <li className={`${props.section === 'hickman' ? 'active' : ''}`}>
-          <a href="./hickman.html">Interactive Hickman Machine</a>
-        </li>
       </ul>
       {props.children}
     </nav>
--- src/components/page_roots/HickmanPage/characterData.js
+++ src/components/page_roots/HickmanPage/characterData.js
@@ -3,13 +3,24 @@ export const characterData = {
     name: "Captain America",
     identity: "Steve Rogers",
     first_appearance: "Avengers (2012) #1",
-    description: `<p>Steve gets to show his strengths as a strategist with a moral
-      center in every aspect of this story. Hickman puts Steve in the toughest
-      position of his life at a cosmic level. In my reading of Hickman's Avengers,
-      Steve is a tragic hero. He is destined to fail against both cosmic and team
-      disintegration. Hickman still makes Steve an inspirational and aspirational
-      character. At least until the final issues of "Time Runs Out" where even he is
-      brought down to trading blows with Tony as the universes collapse.</p>
+    moments: [
+      {
+        text: 'I Am Steve, not Steve-o',
+        issue: 'Avengers #3',
+        type: 'quote'
+      }
+    ],
+    description: `<p>Steve gets to show his strengths as a strategist with moral
+      conviction in every aspect of this story. Hickman shows how this is Cap's best asset
+      and achilles heel. His man-out-of-time condition is
+      taken in a completlely new cosmic direction. Hickman excels at taking what
+      a character represents to move plot and theme. Cap is the best example in this run.</p>
+      
+      <p>In Hickman's Avengers, Steve is a tragic hero. He is destined
+      to fail against both cosmic and team threats. Hickman still makes Steve
+      an inspirational and aspirational character until the final issues of
+      "Time Runs Out". In the end even Cap is brought down to trading blows
+      with Tony as the universes collapse.</p>
       
       <p>During the initial phases of the Illuminati's plans, Steve is the only one
       holding the line at not destroying the incursion planets. Even while in control
@@ -20,16 +31,47 @@ export const characterData = {
       the consequences foir the remainder of the series. The remainder of the Illuminati
       erase his memories of them and move forward with Tony's plan to destroy worlds.</p>
 
-      <p>Steve goes on to continue his plans to make the Avengers a bigger team with Tony,
-      not realizing that plan in itself is partially a ruse by Tony to distract him from
-      the Illuminati's ongoing efforts to destroy worlds at incursion points.</p>
-    `,
-    best_moments: '',
-    quote: ''
+      <p>Steve continues his plans to make the Avengers a bigger team with Tony,
+      not realizing that plan is partially a ruse by Tony to distract him from
+      the Illuminati's ongoing efforts to destroy worlds at incursion points.
+      
+      <p>Steve plays a key leadership role during the cosmic war. He is council with the
+      componay of Ronan, K'lurt, Brood Queen, Guardian. Annhilius and Json of Sparta 
+      during the war agains the beyonders(?). Hickman shows that Steve's true power is
+      as a strategist. Each of these leaders lets Steve's plan take place. And it alwyas
+      works as Cap intends - even if only for a moment before the threat gets bigger.</p>
+
+      <p>Before the story can become Tony vs Steve, Hickman steers Cap into a completely
+      new take on his "man out of time" status. Reverberations from when Steve destroyed
+      the time gem send him and some Avengers forward in time.  Cap relaizes that Tony
+      is wrong - that the incursions are not the end of everything. He's just not certain
+      if its because of what the Illuminati plan to do or not.</p>
+
+      <p>It's here that Hickman takes the biggest swing with Steve. His moral code tells him
+      that even if what the Illuminati are doing saves all remaining life in the multiverses, it
+      is not worth it if it means destroying a single world. Steve doesn't even consider
+      sacrificing the few to save the many. Hickman draws a strong line in the sand that 
+      Steve will never cross.</p>
+
+      <p>Unfortunately for Cap and Tony, fate doesn't work out the way either of them
+      intended. It feels unfinished as the world finally collapses around Steve as he and
+      Tony trade impotent punches. But that's the point. Tony and Steve are almost completely
+      sidelined for the final Secret act of this story after being the focus for so long. The point
+      is that it never mattered in the grand scheme. Steve wasn't punching Tony as much as he
+      was punching Hickman - for tricking him into believing he was the hero up until he
+      faded away.</p>
+    `
   },
   'iron-man': {
     name: "Iron Man",
-    identity: "Tony Stark"
+    identity: "Tony Stark",
+    moments: [
+      {
+        type: 'quote',
+        text: `I can't lie, Bruce... I really am dying to know what's in that case.`,
+        issue: 'Avengers #28'
+      }
+    ]
   },
   'hawkeye': {
     name: "Hawkeye",
@@ -37,7 +79,20 @@ export const characterData = {
   },
   'hulk': {
     name: "Hulk",
-    identity: "Dr. Bruce Banner"
+    identity: "Dr. Bruce Banner",
+    first_appearance: '',
+    moments: [
+      {
+        type: 'quote',
+        style: 'long',
+        text: `
+          This team you assembled... this current version of the Avengers...
+          Why'd you build it this way? I've seen your wheel. Your grid of current
+          and potential members... your avengers machine.
+        `,
+        issue: 'Avengers #28'
+      }
+    ]
   },
   'black-widow': {
     name: "Black Widow",
