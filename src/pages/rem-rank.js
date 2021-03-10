b63f02b6279d63dca5e88c2802fceda072099ef8
Slatron <slatron@gmail.com>
2021-03-10 11:08:47 -0600

    add ie11 polyfills

--- src/pages/rem-rank.js
+++ src/pages/rem-rank.js
@@ -1,3 +1,6 @@
+import 'core-js/stable';
+import "regenerator-runtime/runtime";
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import RemRankPage from 'components/page_roots/RemRankPage/RemRankPage'
