10ee3ad1767cfee023592e09a0cd4389be9957a8
Mike Slater <mike.slater@wellspring.com>
2022-07-21 12:18:53 -0500

    update packages and router

--- src/components/page_roots/AppRouter.js
+++ src/components/page_roots/AppRouter.js
@@ -2,44 +2,26 @@ import React from "react"
 
 import McuRankPage from "./McuRankPage/McuRankPage"
 import ComicsPage from "./ComicsPage/ComicsPage"
-import RemRankPage from "./RemRankPage/RemRankPage"
 import LifetrackerPage from "./LifetrackerPage/LifetrackerPage"
 import AdminPage from "./AdminPage/AdminPage"
 
 import {
-  BrowserRouter as Router,
-  Switch,
-  Redirect,
-  Route
+  BrowserRouter,
+  Routes,
+  Route,
 } from "react-router-dom"
 
 export default function App() {
-
-  let path = localStorage.getItem('redirect-path') || null;
-  if (path) {
-    localStorage.removeItem('redirect-path');
-    return <Redirect to={path} />
-  }
-
   return (
-    <Router>
-      <Switch>
-        <Route path="/lifetracker">
-          <LifetrackerPage />
-        </Route>
-        <Route path="/mcu-rank">
-          <McuRankPage />
-        </Route>
-        <Route path="/rem-rank">
-          <RemRankPage />
-        </Route>
-        <Route path="/admin">
-          <AdminPage />
-        </Route>
-        <Route path="/">
-          <ComicsPage />
-        </Route>
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="lifetracker" element={<LifetrackerPage />} />
+        <Route path="mcu-rank" element={<McuRankPage />} />
+        <Route path="admin" element={<AdminPage />} />
+        <Route path="comics" element={<ComicsPage />} />
+        <Route path="/comics" element={<ComicsPage />} />
+        <Route path="/" element={<ComicsPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
